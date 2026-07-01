843e869d6e7e41f1ef942f04fe93ad2d2d349928
jiayangx <jxudn@connect.ust.hk>
2026-07-01 16:42:46 +0800

    chore(android): bump release versionCode to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.wici.androidalbumdemo"
         minSdk = 33
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "0.1"
     }
 
